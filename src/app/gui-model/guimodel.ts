75d1ace6a3865dd8d0465d54cddba788ee193f49
patrickgardin <gardipat@students.zhaw.ch>
2018-12-11 18:40:19 +0100

    updated

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -1,4 +1,4 @@
-export class GuiModel {
+﻿export class GuiModel {
 
     /* HINWEIS: Texte sind in der Datei ../example-translation-service.ts definiert.
     Erscheinen Texte in {}, so kann die Übersetzung in example-translation-service.ts definiert werden
@@ -6,14 +6,14 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Patricks Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
                     "title": "Friend",
                     "url": "/friend",
-                    "formFieldList": [
-                        {
+                    "formFieldList": [        		
+			{
                             "id": "familyName",
                             "type": "text",
                             "name": "FamilyName",
@@ -27,12 +27,35 @@ export class GuiModel {
                             "width": 1,
                             "required": true
                         },
+			{
+    			    "id": "nickname",
+ 			    "type": "text",
+ 			    "name": "Nickname",
+ 			    "width": 2,
+ 			    "required": true
+			},
+			{
+			    "id": "group",
+ 			    "type": "autocomplete",
+ 			    "name": "Group",
+ 			    "url": "/group",
+ 			    "form": "GroupForm",
+			    "width": 2
+			},	
                         {
                             "id":   "location",
                             "type": "autocomplete",
                             "name": "Location",
                             "url": "/location",
                             "form": "LocationForm",
+                            "width": 2
+                        },
+{
+                            "id":   "activity",
+                            "type": "autocomplete",
+                            "name": "Activity",
+                            "url": "/activity",
+                            "form": "AddActivityForm",
                             "width": 2
                         },
                         {
@@ -89,6 +112,32 @@ export class GuiModel {
                         }
                     ]
                 },
+{
+ "id": "GroupForm",
+ "title": "Group",
+ "url": "/group",
+ "formFieldList": [
+ {
+ "id": "name",
+ "type": "text",
+ "name": "GroupName",
+ "width": 2,
+ "required": true
+ },
+ {
+ "type": "deleteButton",
+ "name": "Delete"
+ },
+ {
+ "type": "cancelButton",
+ "name": "Cancel"
+ },
+ {
+ "type": "okButton",
+ "name": "Ok"
+ },
+ ]
+},
                 {
                     "id": "AddActivityForm",
                     "title": "Activity",
@@ -138,6 +187,20 @@ export class GuiModel {
                             "color": "yellow",
                             "page": "locationspage",
                         },
+			{
+ 			    "type": "button",
+ 			    "name": "Groups",
+ 			    "icon": "fa-weixin",
+ 			    "color": "wisteria",
+ 			    "page": "groupspage",
+			},
+{
+ 			    "type": "button",
+ 			    "name": "Activity",
+ 			    "icon": "fa-weixin",
+ 			    "color": "green",
+ 			    "page": "activitypage",
+			},
                     ]
                 },
                 {
@@ -167,7 +230,60 @@ export class GuiModel {
                         },
                     ]
                 },
-                {
+		{
+ 		    "id": "groupspage",
+ 		    "elementList": [
+ 			{
+ 			    "type": "backbutton",
+ 			},
+ 			{
+ 			    "type": "newButton",
+ 			    "name": "NewGroup",
+ 			    "icon": "fa-weixin",
+ 			    "color": "green",
+ 			    "form": {
+ 				"form": "GroupForm"
+ 			    }
+			{
+			"type": "list",
+			"icon": "fa-weixin",
+			"color": "wisteria",
+			"search": true,
+			"url": "/group",
+			"form": {
+				"form": "GroupForm"
+				}
+ 			},
+		]
+		}
+{
+ 		    "id": "activitypage",
+ 		    "elementList": [
+ 			{
+ 			    "type": "backbutton",
+ 			},
+ 			{
+ 			    "type": "newButton",
+ 			    "name": "NewActivity",
+ 			    "icon": "fa-weixin",
+ 			    "color": "green",
+ 			    "form": {
+ 				"form": "AddActivityForm"
+ 			    }
+			{
+			"type": "list",
+			"icon": "fa-weixin",
+			"color": "wisteria",
+			"search": true,
+			"url": "/activity",
+			"form": {
+				"form": "AddActivityForm"
+				}
+ 			},
+		]
+		}
+
+{
                     "id": "locationspage",
                     "elementList": [
                         {
